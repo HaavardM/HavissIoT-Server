efeb99550874383a43233d0959c3b2ded7ef820f
Håvard S. Mellbye <havardmellbye@gmail.com>
2015-03-29 23:17:49 +0200

    Moving files

---
+++ client/RPI/java/havissIoTClientJava/src/main/java/net/haviss/havissIoTClientJava/havissIoTCLient/messageGenerator.java
@@ -0,0 +1,27 @@
+package net.haviss.havissIoTClientJava;
+
+/**
+ * Created by Håvard on 3/27/2015.
+ */
+public class messageGenerator {
+    //Creates a message containing a temperature value in celsius (float)
+    public String tempCString(String clientID, float tempC) {
+        String message = "@" + clientID + "-TC" + Float.toString(tempC);
+        return message;
+    }
+    //Creates a message containing a temperature value in celsius (int)
+    public String tempCString(String clientID, int tempC) {
+        String message = "@" + clientID + "-TC" + Integer.toString(tempC);
+        return message;
+    }
+    //Creates a message containing a temperature value in fahrenheit (float)
+    public String tempFString(String clientID, float tempF) {
+        String message = "@" + clientID + "-TF" + Float.toString(tempF);
+        return message;
+    }
+    //Creates a message containing a temperature value in fahrenheit (float)
+    public String tempFString(String clientID, int tempF) {
+        String message = "@" + clientID + "-TF" + Integer.toString(tempF);
+        return message;
+    }
+}
