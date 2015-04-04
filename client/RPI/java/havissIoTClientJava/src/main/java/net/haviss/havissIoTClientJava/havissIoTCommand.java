32afd1bd1d3f577cdf580d66d090aa27ddf38f6e
Haavard Mellbye <havardmellbye@gmail.com>
2015-04-04 15:12:29 +0200

    Changed code

--- client/RPI/java/havissIoTClientJava/src/main/java/net/haviss/havissIoTClientJava/havissIoTCommand.java
+++ client/RPI/java/havissIoTClientJava/src/main/java/net/haviss/havissIoTClientJava/havissIoTCommand.java
@@ -11,60 +11,21 @@ import java.util.List;
  * Class gets commands and arguments from string and processes it.
  */
 public class havissIoTCommand {
-    private List<command> commandsBuffer = new ArrayList<command>();
-    private char argumentIdentifier = ':';
-    private char flagIdentifier = '-';
-    //Class for storing command info
-    public static class command {
-        public String flag = "";
-        public String argument = "";
-        public command(String flagString, String argumentString) {
-            flag = flagString;
-            argument = argumentString;
-        }
-    }
-    public void getCommands(String fullstring) {
-
-        List<String> commandStrings = new ArrayList<String>(); //List for storing seperated commandstrings
-        //Seperates the commands into seperate strings from the main string.
-        //Goes through the complete string and seperates commands (each new flag = new commandstring
-        for(int i = 0; i < fullstring.length(); i++) {
-            if(fullstring.charAt(i) == flagIdentifier) {
-                String tempCommandString = "";
-                for(int j = i; j < fullstring.length() && fullstring.charAt(j) != flagIdentifier; j++) {
-                    tempCommandString += fullstring.charAt(j);
-                    i = j;
-                }
-                //Then adds the string into a list of unhandeled commandstrings
-                commandStrings.add(tempCommandString);
+    public String[] getCommands(String fullString) {
+        List<String> tempCmd = new ArrayList<String>();
+        String temp = "";
+        for(int i = 0; i < fullString.length(); i++) {
+            if((fullString.charAt(i) == '-' || fullString.charAt(i) == '$') && i == 0) {
+                tempCmd.add(temp);
+                temp = "";
+                temp += fullString.charAt(i);
             }
+            temp += fullString.charAt(i);
         }
-        //Foreach commandstring in the list - seperate flag and argument and store it into a hashmap.
-        for(String cmdString : commandStrings) {
-            String flag = "";
-            String argument = "";
-            for(int j = 0; j < cmdString.length(); j++) {
-                if(cmdString.charAt(j) == flagIdentifier) {
-                    for(int a = j; a < cmdString.length() && cmdString.charAt(a) != argumentIdentifier; a++) {
-                        flag += cmdString.charAt(j);
-                        j = a;
-                    }
-                }
-                else if(cmdString.charAt(j) == argumentIdentifier) {
-                    for(int a = j; a < cmdString.length(); a++) {
-                        argument += cmdString.charAt(j);
-                        j = a;
-                    }
-                }
-            }
-            commandsBuffer.add(new command(flag, argument)); //Add the command to the commands list
+        if(temp.length() != 0) {
+            tempCmd.add(temp);
         }
-    }
-    //Gets first command in commandbuffer and returns it. Also deletes the command from list
-    public command getNextCommand() {
-        command temp = commandsBuffer.get(0);
-        commandsBuffer.remove(0);
-        return temp;
+        return  tempCmd.toArray(new String[tempCmd.size()]);
     }
 }
 
