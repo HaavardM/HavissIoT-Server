c38993bea1f7047589fdbf10725f1e1091b9551d
Håvard S. Mellbye <havardmellbye@gmail.com>
2015-04-09 09:37:52 +0200

    Changed code for storage handling and added main

--- havissIoT-server/src/main/java/net/haviss/havissIoTServer/Main.java
+++ havissIoT-server/src/main/java/net/haviss/havissIoTServer/Main.java
@@ -4,4 +4,8 @@ package net.haviss.havissIoTServer;
  * Created by Håvard on 3/27/2015.
  */
 public class Main {
+    public static void main(String args[]) {
+
+    }
+
 }
--- havissIoT-server/src/main/java/net/haviss/havissIoTServer/havissIoTStorage.java
+++ havissIoT-server/src/main/java/net/haviss/havissIoTServer/havissIoTStorage.java
@@ -5,27 +5,42 @@ import com.mongodb.MongoClient;
 import com.mongodb.MongoCredential;
 import com.mongodb.ServerAddress;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 /**
  * Created by Håvard on 4/3/2015.
  * This class connects to a mongodb database and handles all storage.
  */
-public class havissIoTStorage {
-
-
-/*-------------------------------------------------------------------------------------------------------------*/
+public class havissIoTStorage implements Runnable {
     //Variables
     private String serverAddress = "";
     private int serverPort = 27017;
+    private List<String> valuesToAdd = new ArrayList<String>();
 
-/*-------------------------------------------------------------------------------------------------------------*/
     //Objects
     private MongoClient mongoClient;
     public DBCollection dbCollection;
 
-/*-------------------------------------------------------------------------------------------------------------*/
     //Functions:
+    @Override
+    public void run() {
+        connect(serverAddress, serverPort);
+        //TODO: Handle code to be excecuted in new thread
+    }
+    //Constructor - stores new values and connects to server
+    public havissIoTStorage(String serverAddress, int serverPort) {
+        this.serverAddress = serverAddress;
+        this.serverPort = serverPort;
+        connect(this.serverAddress, this.serverPort);
+    }
+    //Overloaded constructor - with authentication
+    public havissIoTStorage(String serverAddress, int serverPort, String username, String password, String db) {
+        this.serverAddress = serverAddress;
+        this.serverPort = serverPort;
+        connect(this.serverAddress, this.serverPort, username, password, db);
+    }
     // Connects to server
     public void connect(String address, int port) {
         serverAddress = address;
@@ -39,5 +54,8 @@ public class havissIoTStorage {
     }
     public void getCollection(String collection) {
         dbCollection = dbCollection.getCollection(collection);
+    }
+    public void addValue(String value) {
+        valuesToAdd.add(value); //Adding values to list so the thread can process it.
     }
 }
