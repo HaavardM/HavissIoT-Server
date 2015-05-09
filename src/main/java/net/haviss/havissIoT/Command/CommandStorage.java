107337c302da6090d2bd216c300ac347190473ba
Haavard Mellbye <havardmellbye@gmail.com>
2015-05-09 18:06:12 +0200

    Fixed bug

--- src/main/java/net/haviss/havissIoT/Command/CommandStorage.java
+++ src/main/java/net/haviss/havissIoT/Command/CommandStorage.java
@@ -2,6 +2,8 @@ package net.haviss.havissIoT.Command;
 
 import net.haviss.havissIoT.HavissIoT;
 
+import java.util.Arrays;
+
 /**
  * Created by HaavardM on 5/2/2015.
  * Command to subscribe to topic(s)
@@ -10,15 +12,16 @@ public class CommandStorage implements CommandCallback {
     @Override
     public String run(String[] parameters) {
         StringBuilder builder = new StringBuilder();
+        String[] topics = Arrays.copyOfRange(parameters, 1, parameters.length);
         if(parameters[0].compareTo("-add") == 0) {
-            for (String s : parameters) {
+            for (String s : topics) {
                 HavissIoT.client.subscribeToTopic(s, HavissIoT.client.getQOS());
                 builder.append(" " + s);
             }
             HavissIoT.printMessage("Subscribing to" + builder.toString());
             return "success";
         } else if(parameters[0].compareTo("-remove") == 0) {
-            for(String s : parameters) {
+            for(String s : topics) {
                 HavissIoT.client.unsubscribeToTopic(s);
                 builder.append(" " + s);
             }
