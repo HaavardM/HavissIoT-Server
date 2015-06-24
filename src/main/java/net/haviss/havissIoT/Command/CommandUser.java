b3ebaacde35578d0fa3ce3a8bfc21c65a7bd76cc
Haavard Mellbye <havardmellbye@gmail.com>
2015-06-24 22:50:58 +0200

    Started on user command

---
+++ src/main/java/net/haviss/havissIoT/Command/CommandUser.java
@@ -0,0 +1,81 @@
+package net.haviss.havissIoT.Command;
+
+import com.google.gson.JsonObject;
+import net.haviss.havissIoT.Communication.SocketClient;
+import net.haviss.havissIoT.HavissIoT;
+import net.haviss.havissIoT.Type.User;
+import org.apache.http.HttpStatus;
+import org.reflections.Reflections;
+
+/**
+ * Created by Håvard on 6/24/2015.
+ */
+public class CommandUser implements CommandCallback {
+    private boolean isOP;
+    @Override
+    public String run(JsonObject parameters, User user, SocketClient client) {
+        isOP = user != null && user.isOP();
+        String intent;
+        if(parameters.has("intent")) {
+            intent = parameters.get("intent").getAsString();
+
+            if (intent.compareTo("ADD") == 0) {
+                return newUser(parameters);
+            } else if(intent.compareTo("REMOVE") == 0) {
+
+            }
+        }
+        return null;
+    }
+
+    @Override
+    public String getName() {
+        return "USER";
+    }
+
+    @Override
+    public boolean requireArgs() {
+        return true;
+    }
+
+    private String newUser(JsonObject parameters) {
+        if (isOP) {
+            String name = null;
+            String password = null;
+            boolean OP = false;
+            boolean isProtected = false;
+            User newUser;
+            if (parameters.has("name")) {
+                name = parameters.get("name").getAsString();
+            }
+            if (parameters.has("password")) {
+                password = parameters.get("password").getAsString();
+            }
+            if (parameters.has("isOP")) {
+                OP = parameters.get("isOP").getAsBoolean();
+            }
+            if (parameters.has("isProtected")) {
+                isProtected = parameters.get("isProtected").getAsBoolean();
+            }
+            if (name != null && name.length() > 0) {
+                if (password != null && password.length() > 0) {
+                    newUser = new User(name, password.toCharArray();
+                    newUser.setOP(OP);
+                } else {
+                    newUser = new User(name);
+                }
+                newUser.setProtected(isProtected);
+                if(HavissIoT.userHandler.addUser(newUser)) {
+                    return Integer.toString(HttpStatus.SC_OK)
+                } else {
+                    return Integer.toString(HttpStatus.SC_CONFLICT);
+                }
+            } else {
+                return Integer.toString(HttpStatus.SC_BAD_REQUEST);
+            }
+
+        } else {
+            return Integer.toString(HttpStatus.SC_UNAUTHORIZED);
+        }
+    }
+}
